9c6b18cea8547786e3775a470223582494df76d7
7iYe <15283531823@163.com>
2020-06-23 15:46:32 +0800

    1

--- spring-boot-helloWorld/src/main/java/com/neo/controller/HelloWorldController.java
+++ spring-boot-helloWorld/src/main/java/com/neo/controller/HelloWorldController.java
@@ -8,6 +8,7 @@ public class HelloWorldController {
 	
     @RequestMapping("/hello")
     public String index() {
-        return "Hello World";
+        System.out.println("===========");
+        return "Hello World -------";
     }
 }
